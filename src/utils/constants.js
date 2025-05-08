01f47debcf346fe2b08a36d1f8834c0f494524a5
Manasaey <manasa.sanriya@gmail.com>
2025-05-08 21:25:41 +0530

    sdfjhsjdkfhsd

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
 // src/utils/constants.js
-export const BASE_URL = "https://notes-app-backend-f2b4.onrender.com";
+export const BASE_URL = import.meta.env.VITE_API_URL || "https://notes-app-backend-f2b4.onrender.com";
